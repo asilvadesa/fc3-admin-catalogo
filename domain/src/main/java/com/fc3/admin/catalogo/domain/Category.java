35bdecda7f5757187eb6352419a0f18197d0f660
Anderson Silva de Sa <asilvadesa@gmail.com>
2022-08-29 22:56:50 -0300

    Criando entidade category com TDD

--- domain/src/main/java/com/fc3/admin/catalogo/domain/Category.java
+++ domain/src/main/java/com/fc3/admin/catalogo/domain/Category.java
@@ -1,5 +1,66 @@
 package com.fc3.admin.catalogo.domain;
 
+import java.time.Instant;
+import java.util.UUID;
+
 public class Category {
-   public String id;
+
+   private String id;
+   private String name;
+   private String description;
+   private boolean active;
+   private Instant createdAt;
+   private Instant updateAt;
+   private Instant deleteAt;
+
+
+   private Category(
+           final String id,
+           final String name,
+           final String description,
+           final boolean active,
+           final Instant createdAt,
+           final Instant updateAt,
+           final Instant deleteAt) {
+      this.id = id;
+      this.name = name;
+      this.description = description;
+      this.active = active;
+      this.createdAt = createdAt;
+      this.updateAt = updateAt;
+      this.deleteAt = deleteAt;
+   }
+
+   public static Category newCategory(final String aName, final String aDescription, final boolean isActive){
+      final var now = Instant.now();
+      return new Category(UUID.randomUUID().toString(), aName, aDescription, isActive, now, now, null);
+   }
+
+   public String getId() {
+      return id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public String getDescription() {
+      return description;
+   }
+
+   public boolean isActive() {
+      return active;
+   }
+
+   public Instant getCreatedAt() {
+      return createdAt;
+   }
+
+   public Instant getUpdateAt() {
+      return updateAt;
+   }
+
+   public Instant getDeleteAt() {
+      return deleteAt;
+   }
 }
--- domain/src/test/java/com/fc3/admin/catalogo/domain/CategoryTest.java
+++ domain/src/test/java/com/fc3/admin/catalogo/domain/CategoryTest.java
@@ -6,7 +6,19 @@ import org.junit.jupiter.api.Test;
 public class CategoryTest {
 
     @Test
-    void testNewCategory(){
-        Assertions.assertNotNull(new Category());
+    void givenAValidParams_whenCallNewCategory_thenInstantiateACategory(){
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualCategory =
+                Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
     }
 }
